c85fa3ff0cd6e14df89e94a91cc9d77de3651ba1
jiyuujin <jiyuujinunite@gmail.com>
2022-04-18 17:08:39 +0900

    chore: react18

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    extends: [
-        '@nekohack/eslint-config-react'
-    ],
-    plugins: [
-        '@typescript-eslint'
-    ],
-    root: true,
-    env: { node: true, es6: true },
-    parser: '@typescript-eslint/parser',
-    rules: {
-        'react/display-name': 'off',
-        'react/prop-types': 'off',
-        'jsx-a11y/img-redundant-alt': 'off',
-    }
+  extends: [
+    // '@nekohack/eslint-config-react'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  root: true,
+  env: { node: true, es6: true },
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/img-redundant-alt': 'off',
+  }
 }
